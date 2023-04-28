7f6f97c05f6730114c148208a319e66ad9e2ec11
dtinth on MBP M1 <dtinth@spacet.me>
2023-04-28 19:20:21 +0700

    ‌

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,38 @@
 #!/usr/bin/env node
 
 import yargs from "yargs";
+import { Env } from "@(-.-)/env";
+import { z } from "zod";
+
+const env = Env(
+  z.object({
+    CACHE_TRANSPORTER_TOKEN: z.string(),
+    CACHE_TRANSPORTER_URI: z.string().optional(),
+  })
+);
 
 yargs(process.argv.slice(2))
   .demandCommand()
   .strict()
   .help()
   .command("$0", "push", {}, async () => {
+    if (!env.valid) {
+      console.error("Skipping push, environment is invalid.");
+      console.error(env.error);
+      process.exit(0);
+    }
     console.log("push");
   })
   .command("$0", "pull", {}, async () => {
+    if (!env.valid) {
+      console.error("Skipping pull, environment is invalid.");
+      console.error(env.error);
+      process.exit(0);
+    }
     console.log("pull");
   })
   .command("$0", "server", {}, async () => {
+    const token = env.CACHE_TRANSPORTER_TOKEN;
     console.log("server");
   })
   .parse();
