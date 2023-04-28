b3e8a8812cd35c91096c3fa228193b9090d9ffe4
dtinth on MBP M1 <dtinth@spacet.me>
2023-04-28 19:17:30 +0700

    ‌

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log("meow");
+import yargs from "yargs";
+
+yargs(process.argv.slice(2))
+  .demandCommand()
+  .strict()
+  .help()
+  .command("$0", "Do thing", {}, async () => {
+    console.log("Doing thing");
+  })
+  .parse();
