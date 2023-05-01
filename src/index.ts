e48c33d0ce37d87ca8c8dcbf7263a065daf2be20
dtinth on MBP M1 <dtinth@spacet.me>
2023-05-01 22:13:05 +0700

    ‌

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import tar from "tar";
 import { existsSync, statSync, unlinkSync } from "fs";
 import { fromFileSync } from "hasha";
 import { globbyStream } from "globby";
-import { resolve } from "path";
+import { resolve, relative, sep } from "path";
 
 const networkEnv = Env(
   z.object({
@@ -66,47 +66,9 @@ yargs(process.argv.slice(2))
     async (args) => {
       console.log("save");
 
-      const outFile = "/tmp/nm.tgz";
-      const cwd = "node_modules";
-      const inPaths = ["."];
+      const cacheId = "nm";
 
-      let numberOfFiles = 0;
-      let totalBytes = 0;
-      for await (const entry of globbyStream("**/*", { cwd, dot: true })) {
-        const realPath = resolve(cwd, entry as string);
-        const stat = statSync(realPath);
-        numberOfFiles += 1;
-        totalBytes += stat.size;
-      }
-      console.log("Number of files:", numberOfFiles);
-      console.log("Total bytes:", totalBytes);
-
-      if (existsSync(outFile)) unlinkSync(outFile);
-      const progressReporter = (() => {
-        let count = 0;
-        let last = Date.now();
-        return {
-          tick: () => {
-            count += 1;
-            if (Date.now() - last > 1000) {
-              last = Date.now();
-              console.log("Archiving... Number of files so far:", count);
-            }
-          },
-          finalize() {
-            console.log("Finished archiving. Number of files:", count);
-          },
-        };
-      })();
-      const filter = (path: string) => {
-        // const realPath = resolve(cwd, path);
-        progressReporter.tick();
-        return true;
-      };
-      await tar.c({ gzip: true, file: outFile, cwd, filter }, inPaths);
-      progressReporter.finalize();
-      const hash = fromFileSync(outFile, { algorithm: "sha256" });
-      console.log(hash);
+      await archiveCache(cacheId, ["node_modules"]);
     }
   )
   .command("pull", "pull", {}, async () => {
@@ -126,3 +88,62 @@ yargs(process.argv.slice(2))
     console.log("server");
   })
   .parse();
+
+async function archiveCache(cacheId: string, paths: string[]) {
+  const resolvedPaths = paths.map((path) => resolve(path));
+  const commonAncestor = getCommonAncestor(resolvedPaths);
+  const relativePaths = resolvedPaths.map(
+    (path) => "./" + relative(commonAncestor, path)
+  );
+  const outArchiveFile = `${fsEnv.CACHE_TRANSPORTER_TEMP}/${cacheId}.tgz`;
+
+  const cwd = commonAncestor;
+  const inPaths = relativePaths;
+
+  let numberOfFiles = 0;
+  let totalBytes = 0;
+  for await (const entry of globbyStream("**/*", { cwd, dot: true })) {
+    const realPath = resolve(cwd, entry as string);
+    const stat = statSync(realPath);
+    numberOfFiles += 1;
+    totalBytes += stat.size;
+  }
+  console.log("Number of files found:", numberOfFiles);
+  console.log("Total bytes:", totalBytes);
+  console.log("Creating archive:", outArchiveFile);
+  if (existsSync(outArchiveFile)) unlinkSync(outArchiveFile);
+  const progressReporter = (() => {
+    let count = 0;
+    let last = Date.now();
+    return {
+      tick: () => {
+        count += 1;
+        if (Date.now() - last > 1000) {
+          last = Date.now();
+          console.log("Archiving... Number of files so far:", count);
+        }
+      },
+      finalize() {
+        console.log("Finished archiving. Number of files:", count);
+      },
+    };
+  })();
+  const filter = (path: string) => {
+    // const realPath = resolve(cwd, path);
+    progressReporter.tick();
+    return true;
+  };
+  await tar.c({ gzip: true, file: outArchiveFile, cwd, filter }, inPaths);
+  progressReporter.finalize();
+  const hash = fromFileSync(outArchiveFile, { algorithm: "sha256" });
+  console.log(hash);
+}
+
+function getCommonAncestor(absolutePaths: string[]) {
+  const pairwise = (a: string, b: string) => {
+    return relative(a, b)
+      .split(sep)
+      .reduce((x, y) => (y === ".." ? resolve(x, y) : x), a);
+  };
+  return absolutePaths.reduce(pairwise);
+}
